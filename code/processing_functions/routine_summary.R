87d2bd1d34eeb9ce6e611d463685a9b8cc136ce3
David Pigott <pigottdm@uw.edu>
2020-01-25 15:30:18 -0800

    Added basic sex summary script and first plot

--- code/processing_functions/routine_summary.R
+++ code/processing_functions/routine_summary.R
@@ -1,7 +1,7 @@
 ###################################################################
 ### Code to process recreated line lists to produce githb outputs #
 ###################################################################
-
+library(ggplot2)
 #Unique source compiler
 
 #expects a dataset formatted as per ncov_hubei.csv with x$source
@@ -10,4 +10,28 @@
 sourceCompilR<-function(dataset, citation_format = FALSE, citation_key){
   sources<-dataset$source
   sources<-unique(sources)
+}
+
+sexSummary <- function(dataset, plot = TRUE) {
+  #drop ID = NA, indicative of placeholders
+  dataset <- subset(dataset,!is.na(dataset$ID))
+  sex_string <- dataset$sex
+  total <- length(sex_string)
+  male <- length(sex_string[sex_string == "male"])
+  female <- length(sex_string[sex_string == "female"])
+  unknown <- length(sex_string[sex_string == ""])
+  
+  bar_plot <- data.frame(
+    sex = c("male", "female", "unknown"),
+    count = c(male, female, unknown)
+  )
+  
+  ggplot(data = bar_plot, aes(x = sex, y = count)) +
+    geom_bar(stat = "identity", fill = "steelblue") +
+    geom_text(aes(label = count),
+              vjust = 1.6,
+              color = "white",
+              size = 3.5) +
+    theme_minimal() +
+    ggtitle(paste0("Reported sex ratio = ", round(male/female, 2),":1" ))
 }
