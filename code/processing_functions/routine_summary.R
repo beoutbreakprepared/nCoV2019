5abcbcef15d1055ea64043a628b8c37c456f0a12
David Pigott <pigottdm@uw.edu>
2020-01-25 16:14:42 -0800

    Country summary added to routine code

--- code/processing_functions/routine_summary.R
+++ code/processing_functions/routine_summary.R
@@ -2,6 +2,8 @@
 ### Code to process recreated line lists to produce githb outputs #
 ###################################################################
 library(ggplot2)
+library(gridExtra)
+library(grid)
 #Unique source compiler
 
 #expects a dataset formatted as per ncov_hubei.csv with x$source
@@ -14,7 +16,7 @@ sourceCompilR<-function(dataset, citation_format = FALSE, citation_key){
 
 sexSummary <- function(dataset, plot = TRUE) {
   #drop ID = NA, indicative of placeholders
-  dataset <- subset(dataset,!is.na(dataset$ID))
+  dataset<-subset(dataset, !dataset$country=="")
   sex_string <- dataset$sex
   total <- length(sex_string)
   male <- length(sex_string[sex_string == "male"])
@@ -34,4 +36,21 @@ sexSummary <- function(dataset, plot = TRUE) {
               size = 3.5) +
     theme_minimal() +
     ggtitle(paste0("Reported sex ratio = ", round(male/female, 2),":1" ))
+}
+
+countrySummary<-function(dataset){
+  dataset<-subset(dataset, !dataset$country=="")
+  unique_countries<-unique(dataset$country)
+  
+  summary_output<-data.frame(country = unique_countries, count = rep(NA, length(unique_countries)))
+  for(i in 1:nrow(summary_output)){
+    subset_data<-subset(dataset, dataset$country == summary_output$country[i])
+    summary_output$count[i]<-nrow(subset_data)
+    
+    # if(summary_output$country[i] == "Taiwan, China"){
+    #   summary_output$country[i]<-"Taiwan, Province of China"
+    # }
+  }
+  summary_output<-summary_output[rev(order(summary_output$count)),]
+  grid.table(summary_output)
 }
