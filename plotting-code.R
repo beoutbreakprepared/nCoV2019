f92238c23802a379d4cb9f3bb21ec4578ab46df4
Alex Zarebski <aezarebski@gmail.com>
2020-03-04 15:01:21 +0000

    remove plots and include scatter of delay against symptoms

--- plotting-code.R
+++ plotting-code.R
@@ -70,64 +70,33 @@ sex_age_plot <- ggplot(plot_df, aes(x = bin, y = sum_acc, colour = sex)) +
     ggtitle("Sex stratified age distribution from XXX") +
     chosen_theme
 
+onset_dates <- strpdate(y$date_onset_symptoms)
+conf_dates <- strpdate(y$date_confirmation)
+plot_df <- data.frame(onset_date = onset_dates, delay = conf_dates - onset_dates) %>% filter(!is.na(delay))
+
+delay_plot <- ggplot(plot_df, aes(x = as.Date(onset_date, origin = "1970-01-01"), y = delay)) +
+    geom_jitter(width = 0.5, height = 0.5) +
+    labs(x = "Date of symptom onset", y = "Days after symptom onset for confirmation") +
+    ggtitle("Delay from symptom onset to confirmation XXX") +
+    chosen_theme +
+    theme(axis.text.x = element_text(angle = 0))
 
 
+## > summary(lm(delay ~ onset_date, plot_df))
+## Call:
+##     lm(formula = delay ~ onset_date, data = plot_df)
 
-## tmp1 <- y %>% select(country, date_confirmation)
-## mask <- grepl(pattern = na_string, x = tmp1$date_confirmation)
-## tmp2 <- tmp1[!mask,]
-## mask <- is.na(tmp2$country)
-## tmp2 <- tmp2[!mask,]
-## tmp2$date <- strpdate(tmp2$date_confirmation)
-## total_cases <- tmp2 %>% group_by(country) %>% summarise(num_cases = length(date))
-## ten_case_countries <- total_cases %>% filter(num_cases >= 10) %>% as.data.frame()
-## mask <- sapply(tmp2$country, function(x) x %in% ten_case_countries$country)
-## tmp3 <- tmp2[mask,]
-## tmp4 <- tmp3 %>% group_by(country) %>% summarise(first_case = min(date))
-## tmp5 <- tmp4 %>% select(first_case) %>% unlist() %>% sort.int(index.return = TRUE) %>% `$`("ix")
-## tmp6 <- tmp4$country[tmp5]
+## Residuals:
+##     Min     1Q Median     3Q    Max
+## -5.647 -2.316 -0.411  1.979 10.668
 
-## plot_df <- tmp3 %>% filter(!is.na(country)) %>% filter(country != "NA") %>% mutate(sorted_country = factor(country, levels = tmp6)) %>% left_join(total_cases)
+## Coefficients:
+##     Estimate Std. Error t value Pr(>|t|)
+## (Intercept) 7060.69653  439.12005   16.08   <2e-16 ***
+## onset_date    -0.38586    0.02402  -16.07   <2e-16 ***
+## ---
+##     Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
 
-## cases_through_time <- ggplot(plot_df, aes(x = date, y = sorted_country, fill = log10(num_cases))) +
-##     geom_density_ridges(bandwidth=2,
-##                         jittered_points = TRUE,
-##                         position = position_points_jitter(width = 0.5, height = 0),
-##                         point_shape = '|',
-##                         point_size = 3,
-##                         point_alpha = 1,
-##                         alpha = 0.7,) +
-##     scale_fill_gradient2() +
-##     labs(x = "Date", y = "Country", fill = "Log10 cases") +
-##     ggtitle("Confirmed cases in ten countries with most confirmed cases through time")
-
-
-
-
-## tmp1 <- y %>% select(country, date_onset_symptoms)
-## mask <- grepl(pattern = na_string, x = tmp1$date_onset_symptoms)
-## tmp2 <- tmp1[!mask,]
-## mask <- is.na(tmp2$country)
-## tmp2 <- tmp2[!mask,]
-## tmp2$date <- strpdate(tmp2$date_onset_symptoms)
-## total_cases <- tmp2 %>% group_by(country) %>% summarise(num_cases = length(date))
-## ten_case_countries <- total_cases %>% filter(num_cases >= 3) %>% as.data.frame()
-## mask <- sapply(tmp2$country, function(x) x %in% ten_case_countries$country)
-## tmp3 <- tmp2[mask,]
-## tmp4 <- tmp3 %>% group_by(country) %>% summarise(first_case = min(date))
-## tmp5 <- tmp4 %>% select(first_case) %>% unlist() %>% sort.int(index.return = TRUE) %>% `$`("ix")
-## tmp6 <- tmp4$country[tmp5]
-
-## plot_df <- tmp3 %>% filter(!is.na(country)) %>% filter(country != "NA") %>% mutate(sorted_country = factor(country, levels = tmp6)) %>% left_join(total_cases)
-
-## cases_through_time <- ggplot(plot_df, aes(x = date, y = sorted_country, fill = log10(num_cases))) +
-##     geom_density_ridges(bandwidth=2,
-##                         jittered_points = TRUE,
-##                         position = position_points_jitter(width = 0.5, height = 0),
-##                         point_shape = '|',
-##                         point_size = 3,
-##                         point_alpha = 1,
-##                         alpha = 0.7,) +
-##     scale_fill_gradient2() +
-##     labs(x = "Date", y = "Country", fill = "Log10 cases") +
-##     ggtitle("Onset of symptoms in three countries with most confirmed cases through time")
+## Residual standard error: 3.13 on 476 degrees of freedom
+## Multiple R-squared:  0.3516,	Adjusted R-squared:  0.3502
+## F-statistic: 258.1 on 1 and 476 DF,  p-value: < 2.2e-16
